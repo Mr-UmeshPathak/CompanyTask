6ec29c654ed42a1bcdd0d459dbaeceb531de6af5
Mr-UmeshPathak <umesh979797@gmail.com>
2021-12-22 10:21:02 +0530

    after filtering

--- src/DataFetching.js
+++ src/DataFetching.js
@@ -2,8 +2,11 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function DataFetching() {
-    const [post, setPost] = useState({})
+    const [post, setPost] = useState({});
     const [allEvents, setAllEvents] = useState([]);
+    const [filteredEvents, setFilteredEvents] = useState([]);
+    const [fromDate, setFromDate] = useState();
+    const [toDate, setToDate] = useState();
 
     const url = `https://www.gov.uk/bank-holidays.json`
 
@@ -21,7 +24,8 @@ function DataFetching() {
                         event['division'] =currentDivision;
                     })
                     eventsArray = eventsArray.concat(currentEvents);
-                    setAllEvents(eventsArray)
+                    setAllEvents(eventsArray);
+                    setFilteredEvents(eventsArray);
                 })
             })
             .catch(err => {
@@ -29,9 +33,24 @@ function DataFetching() {
             })
     }, [url])
 
+    const filterData = () =>{
+        let fromDateObject = new Date(fromDate);
+        let toDateObject = new Date(toDate);
+
+        let tempArray = allEvents.filter((event)=>{
+            let eventDateObject = new Date(event.date);
+            return (eventDateObject < toDateObject) && (eventDateObject > fromDateObject);
+        })
+        setFilteredEvents(tempArray)
+    }
+
     return (
         <div className='dataSection'>
             <p> -- Data is here -- </p>
+            <span>From: </span><input type="date" onChange={(e) => setFromDate(e.target.value)} />
+            <span style={{marginLeft:'20px'}}>To: </span><input type="date" onChange={(e) => setToDate(e.target.value)} />
+            <button onClick={filterData} style={{ marginLeft: '20px' }} >Fliter</button>
+            <button onClick={() => setFilteredEvents(allEvents)} style={{ marginLeft: '20px' }} >All Data</button> <br /> <br />
             <table>
                 <tr>
                     <th>Division</th>
@@ -40,7 +59,7 @@ function DataFetching() {
                     <th>Notes</th>
                 </tr>
                 {
-                    allEvents.map((event, i)=>
+                    filteredEvents.map((event, i)=>
                         <tr key={i}>
                             <td>{event.division}</td>
                             <td>{event.title}</td>
